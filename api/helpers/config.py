22163c96715c99e971de0d143d7df010fa3cf81a
marc <mbgraham2000@gmail.com>
2021-05-05 17:06:45 -0700

    wip - update routes in builder move config.py to api/helpers

---
+++ api/helpers/config.py
@@ -0,0 +1,145 @@
+import json
+import os
+import socket
+
+
+class Config:
+    @staticmethod
+    def get_features():
+        features = []
+        if os.path.isfile("/etc/systemd/system/m2ag-thing.service"):
+            features.append('m2ag-thing')
+        if os.path.isfile("/etc/systemd/system/m2ag-indicator.service"):
+            features.append('m2ag-indicator')
+        return features
+
+    @staticmethod
+    def get_server_config():
+        # TODO: add error check here
+        # get server record -
+        # do feature detection. Is motion/mozilla gateway etc installed?
+        config = Config.get_server()
+        config['available'] = Config.get_modules()
+        config['services'] = Config.get_features()
+        with open('./config/enabled.json', 'r') as file:
+            try:
+                enabled = file.read().replace('\n', "")
+                enabled = json.loads(enabled)
+                config['enabled'] = enabled['enabled']
+            except UnicodeDecodeError:
+                config['enabled'] = {}
+
+        # add features covered in separate call.
+        # config['features'] = ConfigHelper.get_features()
+        return config
+
+    @staticmethod
+    def get_config():
+        # get_enabled returns components and things lists
+        config = Config.get_enabled()
+        config['server'] = Config.get_server()
+        config['services'] = Config.get_features()
+        return config
+
+    @staticmethod
+    def get_modules():
+        # all is stored in device/available - dir for each
+        modules = {}
+        entries = os.scandir('./device/available/')
+        for entry in entries:
+            if entry.name == 'helper':
+                continue
+            with open(entry.path, 'r') as file:
+                try:
+                    config = file.read().replace('\n', "")
+                    if config != '':
+                        modules[entry.name[:entry.name.find('.json')]] = json.loads(config)
+                except UnicodeDecodeError:  # .DStore on mac make this choke otherwise
+                    pass
+        return modules
+
+    @staticmethod  # -- 5-27-21
+    def get_thing(thing):
+        with open('./device/available/' + thing + '.json', 'r') as file:
+            config = file.read().replace('\n', "")
+        config = json.loads(config)
+        config['helper'] = Config.get_helper(thing)
+        return config
+
+    @staticmethod
+    def delete_thing(thing):
+        os.remove('./device/available/' + thing + '.json')
+        return os.path.exists('./device/available/' + thing + '.json')
+
+    @staticmethod
+    def put_thing(thing, data):
+        with open('./device/available/' + thing + '.json', 'w') as file:
+            if isinstance(data, dict):
+                data['thing']['init']['title'] = data['thing']['init']['title'].replace('--HOSTNAME--', socket.gethostname())
+                file.write(json.dumps(data))
+            else:
+                file.write(data)
+        file.close()
+        return Config.get_thing(thing)
+
+    @staticmethod  # 5-27-21
+    def get_helper(thing):
+        try:
+            with open('./device/helpers/' + thing + '.py', 'r') as file:
+                config = file.read()
+        except FileNotFoundError:
+            config = 'none'
+        return config
+
+    @staticmethod  # 5-27-21
+    def put_helper(helper, data):
+        with open('./device/helpers/' + helper + '.json', 'w') as file:
+            file.write(data)
+        file.close()
+        return Config.get_helper(helper)
+
+    @staticmethod  # 5-27-21
+    def delete_helper(helper):
+        try:
+            os.remove('./device/helpers/' + helper + '.json')
+        except FileNotFoundError:
+            pass
+        # return os.path.exists('./device/things/' + thing + '.json')
+        return Config.get_helper(helper)
+
+    @staticmethod  # 5-27-21
+    def get_enabled():
+        # get the current map from config.json
+        try:
+            with open('./config/enabled.json', 'r') as file:
+                conf = file.read().replace('\n', "")
+            config = json.loads(conf)
+        except UnicodeDecodeError:
+            config = {'enabled': []}
+        # get lists of available components and things
+        config['available'] = [x.split('.')[0] for x in os.listdir('./device/available/')]
+        config['available'].sort()
+        return config
+
+    @staticmethod  # 5-27-21
+    def put_enabled(enabled):
+        with open('./config/enabled.json', 'w') as file:
+            file.write(json.dumps(enabled))
+        file.close()
+        return Config.get_config()
+
+    @staticmethod
+    def get_server():
+        with open('./config/server.json', 'r') as file:
+            server = file.read().replace('\n', "")
+        server = json.loads(server)
+        return server
+
+    @staticmethod
+    def put_server(server):
+        # TODO: check for valid server file?
+        if server is not None:
+            with open('./config/server.json', 'w') as file:
+                file.write(json.dumps(server))
+            file.close()
+        return Config.get_server()
--- builder.py
+++ builder.py
@@ -19,7 +19,7 @@ CORS(app)
 htpasswd = HtPasswdAuth(app)
 
 
-# jwt to access this thing with
+# jwt to access this thing with 4-27-21
 @app.route('/auth', methods=['GET'])
 @htpasswd.required
 def get_token(user):
@@ -27,7 +27,6 @@ def get_token(user):
         return format_return(Auth.get_token())
 
 
-# manage config files -- 4.27
 # get the whole thing -- limit to get for now since config is assembled
 @app.route('/config', methods=['GET'])
 @htpasswd.required
@@ -38,41 +37,26 @@ def get_config(user):
 
 @app.route('/config/features', methods=['GET'])
 @htpasswd.required
-def get_config_features(user):
+def get_features(user):
     if request.method == 'GET':
         return format_return(Config.get_features())
 
 
 @app.route('/config/server', methods=['GET', 'PUT'])
 @htpasswd.required
-def get_put_server(user):
+def handle_server(user):
     if request.method == 'GET':
         return format_return(Config.get_server())
     elif request.method == 'PUT':
         return format_return(Config.put_server(request.get_json()))
 
     else:
-        return 'access to that service not allowed', 204
+        return 'access to that route not allowed', 204
 
 
-# section = thing or component, component is the file to delete
-@app.route('/config/things/<thing>', methods=['GET', 'PUT', 'DELETE'])
-@htpasswd.required
-def section_component(user, thing):
-    if request.method == 'GET':
-        return format_return(Config.get_module(thing))
-    elif request.method == 'PUT':
-        pass
-        return format_return(Config.put_module(section, component, request.get_json()))
-    elif request.method == 'DELETE':
-        pass
-        return format_return(Config.delete_module(section, component))
-
-
-# 4.27
 @app.route('/config/enabled', methods=['GET', 'PUT'])
 @htpasswd.required
-def get_enabled(user):
+def handle_enabled(user):
     if request.method == 'GET':
         return format_return(Config.get_enabled())
     elif request.method == 'PUT':
@@ -81,35 +65,35 @@ def get_enabled(user):
         return 'access to that route not allowed', 204
 
 
-@app.route('/things/<module>', methods=['GET', 'PUT', 'DELETE'])
+@app.route('/config/things/<thing>', methods=['GET', 'PUT', 'DELETE'])
 @htpasswd.required
-def get_put_thing(user, module):
+def handle_thing(user, thing):
     if request.method == 'GET':
-        return format_return(Config.get_component_thing(module, True))
+        return format_return(Config.get_thing(thing))
     elif request.method == 'PUT':
-        return format_return(Config.put_component_thing(module, request.get_json(), True))
+        return format_return(Config.put_thing(thing, request.get_json()))
     elif request.method == 'DELETE':
-        return format_return(Config.delete_component_thing(module, True))
+        return format_return(Config.delete_thing(thing))
     else:
         return 'access to that service not allowed', 204
 
 
-@app.route('/components/<module>', methods=['GET', 'PUT', 'DELETE'])
+@app.route('/config/helpers/<helper>', methods=['GET', 'PUT', 'DELETE'])
 @htpasswd.required
-def get_put_component(user, module):
+def handle_helpers(user, helper):
     if request.method == 'GET':
-        return format_return(Config.get_component_thing(module, False))
+        return format_return(Config.get_helper(helper))
     elif request.method == 'PUT':
-        return format_return(Config.put_component_thing(module, request.get_json(), False))
+        return format_return(Config.put_helper(helper, request.get_json()))
     elif request.method == 'DELETE':
-        return format_return(Config.delete_component_thing(module, False))
+        return format_return(Config.delete_helper(helper))
     else:
         return 'access to that service not allowed', 204
 
 
-@app.route('/pip/<package>', methods=['GET', 'PUT'])
+@app.route('/config/pip/<package>', methods=['GET', 'PUT'])
 @htpasswd.required
-def get_put_pip(user, package):
+def handle_pip(user, package):
     if request.method == 'GET':
         if package != '--list--':
             return format_return(Utils.get_pip(package))
@@ -125,8 +109,8 @@ def get_put_pip(user, package):
 
 @app.route('/<service>/<action>', methods=['GET'])
 @htpasswd.required
-def service_action(user, service, action):
-    if service in ['m2ag-thing', 'm2ag-motion', 'm2ag-homeassistant', 'm2ag-indicator', 'm2ag-gateway']:
+def handle_service(user, service, action):
+    if service in ['m2ag-thing', 'm2ag-indicator']:
         # the service web component prefixes everything with m2ag-
         if service == 'm2ag-motion':
             return format_return(Utils.service_action('motion', action))
@@ -138,7 +122,7 @@ def service_action(user, service, action):
 
 @app.route('/password', methods=['PUT', 'GET', 'POST', 'DELETE'])
 @htpasswd.required
-def password(user):
+def handle_password(user):
     if request.method == 'PUT':
         return format_return(Password.change_password(request.get_json()))
     if request.method == 'GET':
@@ -152,12 +136,11 @@ def password(user):
 def format_return(data):
     return Response(json.dumps({'data': data}), mimetype='application/json')
 
-
+# TODO: wsgi <-- tornado
 if __name__ == '__main__':
     if os.path.isfile(f'{str(Path.home())}/.m2ag-labs/ssl/server.crt') and os.path.isfile(
             f'{str(Path.home())}/.m2ag-labs/ssl/server.key'):
         context = (f'{str(Path.home())}/.m2ag-labs/ssl/server.crt', f'{str(Path.home())}/.m2ag-labs/ssl/server.key')
         app.run(host='0.0.0.0', port='5000', ssl_context=context)
-    #  app.run(host=f'{socket.gethostname()}.local', port='5000', ssl_context=context)
     else:
         app.run(host='0.0.0.0', port='5000')
